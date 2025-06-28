232ba1846c4badc0e36c5ff2482ff22b81685c53
Fatima740227 <fatima3.iscool@gmail.com>
2025-06-27 16:45:28 -0800

    commit 1

---
+++ Module 1/Activity 1.py
@@ -0,0 +1,21 @@
+name = "Fatima"
+age = 12
+is_student = True
+weight = 41.1
+
+print ("Name :", name)
+print ("Data Type of Name is", type(name))
+print ("Age :", age)
+print ("Data Type of Age is", type(age))
+print ("Age :", is_student)
+print ("Data Type of Age is", type(is_student))
+print ("Age :", weight)
+print ("Data Type of Age is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = str(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
