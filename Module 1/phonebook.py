60dfec4396c3f96c0bbc5285651eadcc9d5ce29d
Fatima740227 <fatima3.iscool@gmail.com>
2025-07-31 14:44:34 -0800

    initial commit

--- Module 1/phonebook.py
+++ Module 1/phonebook.py
@@ -1,7 +1,62 @@
+import sys 
 # this function will  be the first to run as the main function executes
 def initial_phonebook():
+      rows, cols = int(input("Please enter initial number of contacts.")), 5
+      phone_book = []
+      print(phone_book)
+      for i in range(rows):
+            print("\nEnter contact %d details in the following order (ONLY):" % (i+1))
+            print("Note: * indicates mandatory fields")
+
+            print("........................................")
+            temp = []
+            for j in range(cols):
+                   if j == 0:
+                        temp.append (str(input("Enter Name*: ")))
+                        if temp[j] == '' or temp[j] == '':
+                              sys.exit
+                              "Name is mandatory field process exiting due to blank field"
+                   if j == 1:
+                         temp.append (int(input("Enter Number*: ")))
+                   if j == 2:
+                        temp.append (str(input("Enter E-mail address*: ")))
+                        if temp[j] == '' or temp[j] == '':
+                              temp[j] = None
+                   if j == 3:
+                        temp.append (str(input("Enter Date of Birth(dd/mm/yy): ")))
+                        if temp[j] == '' or temp[j] == '':
+                              temp[j] = None
+                   if j == 4:
+                         temp.append (
+                             str(input("Enter Category(Family,Friends,Work,Others): ")))
+                         if temp[j] == '' or temp[j] == '':
+                                  temp[j] = None
+            phone_book.append(temp)
+      print(phone_book)
+      return phone_book
+                                 
+                        
+                          
+                          
+                        
 
 def menu():
+      print("**********************************************************************************")
+      print("\t\t\tSMARTPHONE DIRECTORY", flush=False)
+      print("**********************************************************************************")
+      print("\t You can perform the following operations in the phone book\n")
+      print("1.Add  a new  contact")
+      print("2.Rem ove  an existing contact")
+      print("3.Delete all  contacts")
+      print("4.Search  for a  contact")
+      print("5.Display all contacts")
+      print("6.Exit Phonebook")
+
+      choice= int(input("Please enter your  choice"))
+      return  choice
+
+
+
 def add_contact(pb):
 def remove_existing(pb):
 def delete_all(pb):
